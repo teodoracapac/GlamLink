c8ec11493bd65ac1d0d9f4b80fc9850bea312895
Capac Teodora <teodora.capac@yahoo.com>
2025-04-29 16:19:08 +0300

    login cu asp + db

---
+++ login_asp/Models/Admins.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationGlamLink.Models
+{
+    public class Admins
+    {
+        public int IdAdmin { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ login_asp/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationGlamLink.Models
+{
+    public class Users
+    {
+        public int IdUsers { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
